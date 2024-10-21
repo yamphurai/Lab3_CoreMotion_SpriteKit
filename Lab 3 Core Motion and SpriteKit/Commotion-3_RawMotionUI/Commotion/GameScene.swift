8c920888d0c889cc7c8d6dca4581a36c5f99947a
Jeevan Rai <yamphurai007@yahoo.com>
2024-10-20 22:01:50 -0500

    Adding module B

---
+++ Lab 3 Core Motion and SpriteKit/Commotion-3_RawMotionUI/Commotion/GameScene.swift
@@ -0,0 +1,13 @@
+//
+//  GameScene.swift
+//  Commotion
+//
+//  Created by Reshma Shrestha on 10/19/24.
+//  Copyright © 2024 Eric Larson. All rights reserved.
+//
+
+import UIKit
+
+class GameScene: SKScene {
+
+}
---
+++ Lab 3 Core Motion and SpriteKit/Commotion-3_RawMotionUI/Commotion/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  Commotion
+//
+//  Created by Reshma Shrestha on 10/19/24.
+//  Copyright © 2024 Eric Larson. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
